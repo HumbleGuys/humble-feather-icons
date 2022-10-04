1540add61651da8169474f654de97cac5968dedd
Kim Larsson <kim@humbleguys.se>
2022-10-04 23:41:20 +0200

    Removed temp code for creating icons

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -13,14 +13,5 @@ class ServiceProvider extends SupportServiceProvider
 
     public function boot(): void
     {
-        /*
-        collect(app('files')->files(__DIR__.'/resources/icons'))->each(function ($file) {
-            $new = __DIR__.'/resources/components/'.str($file->getFilename())->beforeLast('.').'.blade.php';
-
-            if (! file_exists($new)) {
-                copy($file->getRealPath(), $new);
-            }
-        });
-        */
     }
 }
